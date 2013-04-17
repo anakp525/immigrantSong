f1f300f84d08064a47839dadae919b306c618dff
Nicholas Melnyk <nicholasmelnyk@Nicholass-MacBook-Pro.local>
2013-04-17 18:37:22 -0500

    US Data

--- createTotals.py
+++ createTotals.py
@@ -1,20 +1,31 @@
 import sys
 import json
 import os
+import csv
 
 def createBy(data, how):
 	fileName = how + '.csv'
 	# Get headings
-	headings = ['year', 'collectionType', ']
+	headings = ['year', 'collectionType', '']
 	with open(fileName, 'wb') as file:
 		writer = csv.writer(file)
 		for state in data:
+			continue
 		
-	file.close()
-	pass
+	#file.close()
 
 def mergeAll():
 	pass
+
+def writeCSV(info):
+	with open('US.csv', 'wb') as file:
+		writer = csv.writer(file)
+		for row in info:
+			line = []
+			for value in row:
+				line.append(value)
+			writer.writerow(line)
+		file.close()
 
 def createCSVFiles(data):
 	createBy(data, 'state of residence')
@@ -23,13 +34,139 @@ def createCSVFiles(data):
 	createBy(data, 'country of birth')
 	mergeAll()
 	
+def getTotal(row):
+	total = 0.0
+	m = row['m']
+	f = row['f']
+	u = row['u']
+	return m+f+u
+
+def dataByState(data, type = 'State of Residence'):
+	d = {}
+	US = []
+	numberImmigrants = 0
+	for year in data:
+		for region in data[year][type]:
+			if region == 'totals':
+				continue
+			if region not in d:
+				d[region] = {}
+			d[region][year] = {}
+			for cat in data[year][type][region]:
+				if cat == 'total':
+					m = data[year][type][region][cat]['m']
+					f = data[year][type][region][cat]['f']
+					u = data[year][type][region][cat]['u']
+					US.append( (region, year, 'Number of immigrants:', {'u':u, 'f':f, 'm':m}) )
+					continue
+				if cat not in d[region][year]:
+					d[region][year][cat] = {}
+				topValue = ('blah', -100)
+				numberImmigrants = 0
+				for value in data[year][type][region][cat]:
+					if value not in d[region][year][cat]:
+						d[region][year][cat][value] = {}
+					total = getTotal(data[year][type][region][cat][value])
+					numberImmigrants = numberImmigrants + total
+					if total > topValue[1]:
+						topValue = (value, total)
+					for gender in data[year][type][region][cat][value]:
+						if gender not in d[region][year][cat][value]:
+							d[region][year][cat][value][gender] = data[year][type][region][cat][value][gender]
+					
+					m = data[year][type][region][cat][value]['m']
+					f = data[year][type][region][cat][value]['f']
+					u = data[year][type][region][cat][value]['u']
+				#print numberImmigrants
+				if numberImmigrants == 0:
+					US.append( (region, year, cat, topValue, 100.0) )
+				else:
+					US.append( (region, year, cat, topValue, float(topValue[1]/numberImmigrants)*100) )
+				#US.append( (region, year, cat, value, topValue, total""", {'u':u, 'f':f, 'm':m}""") )
+	
+	#for key in sorted(US, key=lambda x:x[0]):
+	#	print key
+	
+	return US 
+
+def convertToDict(l):
+	rows = []
+	states = {}
+	for item in l:	# 5 tuple - region, year, category, (top Category, value), percentage
+		row = ()
+		state = item[0]
+		year = item[1]
+		immigrants = 0.0
+		maleImmigrants = 0.0
+		femaleImmigrants = 0.0
+		topCategory = ''
+		topValue = ''
+		if state not in states:
+			states[state] = {}
+		if year not in states[state]:
+			states[state][year] = {}
+	
+		category = item[2]
+		if category == 'Number of immigrants:':
+			immigrants = item[3]['m'] + item[3]['f'] + item[3]['u']
+			maleImmigrants = item[3]['m']
+			femaleImmigrants = item[3]['f']
+			if category not in states[state][year]:
+				states[state][year][category] = {}
+			states[state][year][category]['m'] = item[3]['m']
+			states[state][year][category]['f'] = item[3]['f']
+			states[state][year][category]['total'] = immigrants
+		else:
+			topCategory = item[3][0]
+			topValue = item[3][1]
+			if category not in states[state][year]:
+				states[state][year][category] = {}
+			if topCategory not in states[state][year][category]:
+				states[state][year][category][topCategory] = topValue
+		row = (state, year, immigrants, maleImmigrants, femaleImmigrants, category, topCategory, topValue) 
+		rows.append(row)
+
+	sortedRows = sorted(rows, key=lambda x:x[0])
+	actual = []
+	#concatenate Rows
+	for i in range(len(sortedRows)):
+		row = sortedRows[i]
+		state = row[0]
+		if sortedRows[i+1][0] == state:	# Same state. Concatenate values
+			pass	
+		else:				# Last state
+			pass
+			
+		print row
+	#for k in states:
+	#	print k, states[k]
+	return states
+	pass
+
+def createExcel(dict):
+	states = []
+	for state in dict:
+		row = ()
+		#for year in sorted(dict[state], key=lambda x:x[0]):
+		#	print state, year
+	pass
+
 def main():
 	file = open('data.json', 'r')
 	line = file.readline()
 	file.close()
 	DHS = json.loads(line)	
 	
-	createCSVFiles(DHS)
-	print DHS
+	#createCSVFiles(DHS)
+	byState = dataByState(DHS)
+	writeCSV(byState)
+	#print len(byState)	
+	#for k in sorted(byState, key=byState.get):
+	#	print k
+	#d = convertToDict(byState)
+	#createExcel(d)
+
+	byCountry = dataByState(DHS, 'Country of Birth')
+	#print DHS
 
 main()
